d61bf2a3c1ab8f1065b5387e2836ede013577965
Fatiahalzahra <114464481+Fatiahalzahra@users.noreply.github.com>
2023-02-27 09:46:36 +0700

    praktikum1

---
+++ Praktikum/percabangan.php
@@ -0,0 +1,15 @@
+<?php
+$jam = 23;
+
+echo "saat ini pukul: $jam <br>";
+
+$hasil = $jam < 12;
+
+if($hasil) {
+    echo "Selamat Pagi!";
+}elseif ($jam < 18) {
+    echo "Selamat Sore!";
+} else {
+    echo "Selamat Malam!";
+}
+
