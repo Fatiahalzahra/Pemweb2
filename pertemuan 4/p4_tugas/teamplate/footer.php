5e6a371c32b177a0e80483aa8511296dbd6a4722
Fatiahalzahra <fatiahalzahra2003@gmail.com>
2023-03-21 16:22:21 +0700

    fatiah_TI07

---
+++ pertemuan 4/p4_tugas/teamplate/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Fatiah Al Zahra TI_07 &copy; @fatiahalzahra2003@gmail.com</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.0
+    </div>
+</footer>
